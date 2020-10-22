d3099b505740424b8bb237d928d5fd477a3037d7
Josh Soref <jsoref@users.noreply.github.com>
2020-10-21 20:32:37 -0400

    spelling: deprecated
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- app/src/main/java/org/thoughtcrime/securesms/preferences/AppProtectionPreferenceFragment.java
+++ app/src/main/java/org/thoughtcrime/securesms/preferences/AppProtectionPreferenceFragment.java
@@ -363,7 +363,7 @@ public class AppProtectionPreferenceFragment extends CorrectedPreferenceFragment
     }
   }
 
-  // Derecated
+  // Deprecated
 
   private class ChangePassphraseClickListener implements Preference.OnPreferenceClickListener {
     @Override
